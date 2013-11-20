229b18ea5679f6dd683aaa9fa797c17feea16af4
phoenixbox <shane@quickleft.com>
2013-11-19 21:19:15 -0700

    concentric circles logic

--- Hypnosister/HypnosisterView.m
+++ Hypnosister/HypnosisterView.m
@@ -19,15 +19,20 @@
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
-    float maxRadius = hypot(bounds.size.width, bounds.size.height)/ 4.0;
+    float maxRadius = hypot(bounds.size.width, bounds.size.height)/ 2.0;
     
     CGContextSetLineWidth(ctx, 10);
     
     CGContextSetRGBStrokeColor(ctx, 0.6, 0.6, 0.6, 1.0);
     
-    CGContextAddArc(ctx, center.x, center.y, maxRadius, 0.0, M_PI * 2.0, YES);
+//    CGContextAddArc(ctx, center.x, center.y, maxRadius, 0.0, M_PI * 2.0, YES);
     
-    CGContextStrokePath(ctx);
+//    CGContextStrokePath(ctx);
+    
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20){
+        CGContextAddArc(ctx, center.x, center.y, currentRadius, 0.0, M_PI * 2.0, YES);
+        CGContextStrokePath(ctx);
+    }
 }
 
 - (id)initWithFrame:(CGRect)frame
